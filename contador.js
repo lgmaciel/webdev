6242b1e5251401ca3fa1803d7226d1d9d97fe28b
lgmaciel <132719840+lgmaciel@users.noreply.github.com>
2025-10-21 21:44:51 -0300

    refatorado e incrementado

--- contador.js
+++ contador.js
@@ -1,10 +1,41 @@
 const numContador = document.querySelector("#num-contador")
-let contagem = numContador.value
+const numX10 = document.querySelector("#num-x10")
 
-function contar() {
-    const numContador = document.querySelector("#num-contador")
+let contagem = numContador.value
+let contagemX10 = numX10.value
+
+function contar() {    
     numContador.value = ++contagem
+}
+
+function zerar() {
+    contagem = 0
+    contagemX10 = 3
+
+    numContador.value = 0
+    numX10.value = contagemX10
+}
+
+function x10() {
+    if(contagemX10>0){
+        contagemX10--
+
+        numContador.value = contagem *= 10    
+        numX10.value = contagemX10
+    } 
+    if(contagemX10==0) {
+        const btX10 = document.querySelector("#bt-x10")
+        btX10.disabled = "true"
+    }
+    
 }
 
 const btContar = document.querySelector("#bt-contar")
 btContar.addEventListener("click", contar)
+
+const btZerar = document.querySelector("#bt-zerar")
+btZerar.addEventListener("click", zerar)
+
+const btX10 = document.querySelector("#bt-x10")
+btX10.addEventListener("click", x10)
+
